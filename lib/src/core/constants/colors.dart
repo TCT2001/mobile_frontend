fd2047433e15659d5092719a721edbee3b4fc6e4
trnminhngc <tmngoc2001hn@gmail.com>
2021-11-15 09:58:38 +0700

    colors

--- lib/src/core/constants/colors.dart
+++ lib/src/core/constants/colors.dart
@@ -10,4 +10,6 @@ const Color DenimBlue = Color(0xff2d5f79);
 const Color Betsy = Color(0xff73c6d9);
 const Color BathWater = Color(0xff88e8f2);
 const Color Bg = Color(0xffffffff);
+const Color Red = Color (0xffff0000);
+
 
