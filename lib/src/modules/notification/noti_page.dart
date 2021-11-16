05a48c6902abdb280070ea941a6d8af221e064da
tuyen <minhtuyen300920011@gmail.com>
2021-11-16 17:10:04 +0700

    fix narbar'

---
+++ lib/src/modules/notification/noti_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mobile_app/src/global_widgets/navbar.dart';
+import 'noti_controller.dart';
+
+class NotiPage extends GetView<NotiController> {
+  NotiPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          elevation: 20,
+          title: const Text('Notification'),
+        ),
+        body: SafeArea(
+          child: Center(child: Text("This is Notification Page")),
+        ),
+        bottomNavigationBar: Narbar());
+  }
+}
