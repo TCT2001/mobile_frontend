e707ca0a2d703816d454af94936a20109578e7b5
tuyen <minhtuyen300920011@gmail.com>
2021-11-18 09:07:35 +0700

    fix

--- lib/src/modules/setting/setting_page.dart
+++ lib/src/modules/setting/setting_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:settings_ui/settings_ui.dart';
 import 'setting_controller.dart';
 
 class SettingPage extends GetView<SettingController> {
@@ -7,7 +8,30 @@ class SettingPage extends GetView<SettingController> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("This is Setting Page"));
+    bool _a = true;
+    return Center(child: SettingsList(
+        sections: [
+          SettingsSection(
+            title: 'Section',
+            tiles: [
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: const Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+              SettingsTile.switchTile(
+                title: 'Use fingerprint',
+                leading: Icon(Icons.fingerprint),
+                switchValue: _a,
+                onToggle: (bool value) {
+                  _a = !_a;
+                },
+              ),
+            ],
+          ),
+        ],
+      ));
   }
 
 }
