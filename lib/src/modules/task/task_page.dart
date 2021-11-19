f87d773ac78fb853c1a5c62df22f87112b381e5b
Thanh-Van-2001 <felicenguyen2001@gmail.com>
2021-11-19 16:02:03 +0700

    login screen 2.0 da chinh UI

--- lib/src/modules/task/task_page.dart
+++ lib/src/modules/task/task_page.dart
@@ -9,6 +9,37 @@ class TaskPage extends GetView<TaskController> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("This is Tasks Page"));
+    return Scaffold(
+      body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blueGrey, Colors.lightBlueAccent]),
+          ),
+          child: Padding(
+              padding: const EdgeInsets.all(125),
+
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: const [
+                      Text("This is task page",
+                          style: TextStyle(
+                              color: Color(0xff88e8f2),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18)),
+                      SizedBox(height: 8),
+
+                      SizedBox(height: 8),
+
+
+                    ]),
+              )
+          )),
+    )
+    ;
   }
 }
