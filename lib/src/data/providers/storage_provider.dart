d1da3ece878d498cd25a38eb9de8f17810082191
Tuyen <minhtuyen300920011@gmail.com>
2021-11-27 10:49:35 +0700

    fix logout

--- lib/src/data/providers/storage_provider.dart
+++ lib/src/data/providers/storage_provider.dart
@@ -4,14 +4,13 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
 Future<String?> getStringLocalStorge(String key) async {
-  //TODO
   final SharedPreferences prefs = await _prefs;
   return prefs.getString(key);
 }
 
 void setStringLocalStorge(String key, String value) async {
   final SharedPreferences prefs = await _prefs;
-  prefs.setString(key, value);
+  await prefs.setString(key, value);
 }
 
 Future<int?> getIntLocalStorge(String key) async {
@@ -21,5 +20,15 @@ Future<int?> getIntLocalStorge(String key) async {
 
 void setIntLocalStorge(String key, int value) async {
   final SharedPreferences prefs = await _prefs;
-  prefs.setInt(key, value);
+  await prefs.setInt(key, value);
+}
+
+void deleteLocalStorageByKey(String key) async {
+  final SharedPreferences prefs = await _prefs;
+  await prefs.remove(key);
+}
+
+void cleanLocalStorage() async {
+  final SharedPreferences prefs = await _prefs;
+  await prefs.clear();
 }
